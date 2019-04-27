627c1de8e9a75a52691596eef8acd400689eec00
3TUSK <urey.s.knowledge@gmail.com>
2019-04-26 23:57:40 -0700

    Bullet work

--- bullet.gd
+++ bullet.gd
@@ -3,13 +3,11 @@ extends RigidBody2D
 # Member variables
 var disabled = false
 
-
 func disable():
 	if disabled:
 		return
 	$anim.play("shutdown")
 	disabled = true
-
 
 func _ready():
 	$Timer.start()
