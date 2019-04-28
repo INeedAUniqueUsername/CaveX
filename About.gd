268f9456ba221e6b03d5891bdf05f23eb962d762
INeedAUniqueUsername <achen115@ucr.edu>
2019-04-27 23:54:10 -0700

    add about and font license

---
+++ About.gd
@@ -0,0 +1,12 @@
+extends Control
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _back():
+	get_tree().change_scene("res://intro.tscn")
